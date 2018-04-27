7dadd0943bed2151dd8476689d699ff0a1ccb69b
Ronan Lina <ronanlina@gmail.com>
2018-04-27 15:47:31 +0800

    #Real-time identification  - pushed 3 unversioned files

---
+++ app/src/main/java/com/example/connivingdog/numberguess/AddSimPref.kt
@@ -0,0 +1,12 @@
+package com.example.connivingdog.numberguess
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AddSimPref : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_sim_pref)
+    }
+}
