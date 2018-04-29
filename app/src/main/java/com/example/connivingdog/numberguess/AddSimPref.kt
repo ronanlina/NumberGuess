c3f152149e041a3fb81fc6d730da851bcdf3e164
Ronan Lina <ronanlina@gmail.com>
2018-04-29 22:56:59 +0800

    #revert

--- app/src/main/java/com/example/connivingdog/numberguess/AddSimPref.kt
+++ app/src/main/java/com/example/connivingdog/numberguess/AddSimPref.kt
@@ -3,11 +3,15 @@ package com.example.connivingdog.numberguess
 import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.CoordinatorLayout
+import android.support.design.widget.Snackbar
 import android.view.KeyEvent
 import android.view.View
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_add_sim_pref.*
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import com.example.connivingdog.numberguess.R.id.*
 import com.google.firebase.database.*
 
 
@@ -32,47 +36,91 @@ class AddSimPref : AppCompatActivity() {
         prefText.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
             if(event.action == KeyEvent.ACTION_DOWN){
                 if (keyCode == KeyEvent.KEYCODE_ENTER) {
-                    attemptAddPrefix()
+                    //attemptAddPrefix()
+                    addPrefix()
                     return@OnKeyListener true
                 }
             }
             false
         })
+
     }
 
-    private fun attemptAddPrefix(){
-        var cancel: Boolean = false
-        var focusView: View? = null
+    /*private fun attemptAddPrefix(){
         var simPref: String = prefText.text.toString()
+        var sp: SimPrefix
 
         mDatabaseReference.child("simnumbers")
                 .orderByChild("simPref")
                 .equalTo(simPref)
+                .limitToFirst(1)
                 .addValueEventListener( object : ValueEventListener {
                     override fun onDataChange(dataSnapshot: DataSnapshot) {
-                        if(dataSnapshot.hasChildren()){
-                            prefText.error = "prefix already exists!"
-                            cancel = true
-                            focusView = prefText
+
+                        if(dataSnapshot!=null){
+                            dataSnapshot.children.forEach {
+                                sp = it.getValue(SimPrefix::class.java)!!
+                                if(sp.simPref.toString().equals(simPref)){
+                                    Toast.makeText(this@AddSimPref, "already exists", Toast.LENGTH_SHORT).show()
+                                }
+                                else{
+                                    addPrefix()
+                                }
+                            }
                         }
+                        else{
+                            Toast.makeText(this@AddSimPref, "else", Toast.LENGTH_SHORT).show()
+                        }
+
                     }
                     override fun onCancelled(databaseError: DatabaseError) {
                         println("loadPost:onCancelled ${databaseError.toException()}")
                     }
                 })
-
-        if(cancel){
-            focusView?.requestFocus()
-        }
-        else{
-            addPrefix()
-        }
-    }
+    }*/
     //push to database
     private fun addPrefix(){
         var simCard = cardSpin.selectedItem.toString()
         var simPref: String = prePrefView.text.toString() + prefText.text.toString()
         var sp = SimPrefix(simCard,simPref)
         mDatabaseReference.child("simnumbers").push().setValue(sp)
+        Toast.makeText(this,"sim card prefix added!", Toast.LENGTH_SHORT).show()
+//        var coordinator: CoordinatorLayout? = R.id.coordinator as CoordinatorLayout?
+//        val snackbar = Snackbar
+//                .make(coordinator!!, "Snackbar with Callback", Snackbar.LENGTH_LONG)
+//                .setAction("OK") {
+//                    val snackbar1 = Snackbar.make(coordinator, "Snackbar with Callback called.", Snackbar.LENGTH_SHORT)
+//                    snackbar1.show()
+//                }
     }
 }
+
+
+
+//mDatabaseReference.child("simnumbers")
+//                .addValueEventListener( object : ValueEventListener {
+//                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+//
+//                        var focusView: View? = null
+//
+//                        var exists: Boolean = false
+//                        dataSnapshot.children.forEach {
+//                            sp = it.getValue(SimPrefix::class.java)!!
+//                            if(sp.simPref.equals(simPref)){
+//                                exists = true
+//                                prefText.error = "prefix already exists!"
+//                                focusView = prefText
+//                            }
+//                        }
+//
+//                        if(exists){
+//                            focusView?.requestFocus()
+//                        }
+//                        else{
+//                            addPrefix()
+//                        }
+//                    }
+//                    override fun onCancelled(databaseError: DatabaseError) {
+//                        println("loadPost:onCancelled ${databaseError.toException()}")
+//                    }
+//                })
