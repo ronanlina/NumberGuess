ea4a455deba5ee8b610bde5d8657d8c5b777e21e
Ronan Lina <ronanlina@gmail.com>
2018-05-02 10:26:10 +0800

    #installed crashlytics

--- app/src/main/java/com/example/connivingdog/numberguess/MainActivity.kt
+++ app/src/main/java/com/example/connivingdog/numberguess/MainActivity.kt
@@ -14,6 +14,12 @@ import kotlinx.android.synthetic.main.activity_main.*
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.DataSnapshot
+import com.crashlytics.android.Crashlytics;
+import com.example.connivingdog.numberguess.R.drawable.sun
+import io.fabric.sdk.android.Fabric;
+//import sun.security.krb5.internal.KDCOptions.with
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,6 +27,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Fabric.with(this, Crashlytics())
         setContentView(R.layout.activity_main)
         //grid button onClick listeners (not simplified)
         b1.setOnClickListener{numberText.text.append(b1.text) }
